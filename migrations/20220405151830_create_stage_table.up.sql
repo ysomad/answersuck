4c39b73455b7da9486fc18dfe4078a28493c5ec5
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-04-15 20:31:45 +0700

    refactoring: increased length of stage name

--- migrations/20220405151830_create_stage_table.up.sql
+++ migrations/20220405151830_create_stage_table.up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS stage
 (
     id         serial             NOT NULL PRIMARY KEY,
-    name       varchar(24)        NOT NULL,
+    name       varchar(32)        NOT NULL,
     is_final   bool DEFAULT FALSE NOT NULL,
     "order"    int                NOT NULL,
     package_id int                NOT NULL REFERENCES package (id)
