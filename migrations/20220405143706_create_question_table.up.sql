75e4d29bf9c443f5011bed957dc4122bbe7bc769
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-04-15 23:30:57 +0700

    refactoring: added create_at field to topic and question for filters

--- migrations/20220405143706_create_question_table.up.sql
+++ migrations/20220405143706_create_question_table.up.sql
@@ -9,10 +9,11 @@ CREATE TABLE IF NOT EXISTS question_media
 
 CREATE TABLE IF NOT EXISTS question
 (
-    id          serial       NOT NULL PRIMARY KEY,
-    question    varchar(255) NOT NULL,
-    answer_id   int          NOT NULL REFERENCES answer (id),
-    account_id  uuid         NOT NULL REFERENCES account (id),
-    language_id int          NOT NULL REFERENCES language (id),
-    media_id    int REFERENCES question_media (id)
+    id          serial                                             NOT NULL PRIMARY KEY,
+    question    varchar(255)                                       NOT NULL,
+    answer_id   int                                                NOT NULL REFERENCES answer (id),
+    account_id  uuid                                               NOT NULL REFERENCES account (id),
+    language_id int                                                NOT NULL REFERENCES language (id),
+    media_id    int REFERENCES question_media (id),
+    created_at  timestamp WITH TIME ZONE DEFAULT current_timestamp NOT NULL
 );
