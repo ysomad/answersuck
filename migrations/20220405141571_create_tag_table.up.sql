57a6ff8cb72e0890f0349b81d2f0cef0997dbbc2
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-04-15 20:03:12 +0700

    refactoring: fixed migrations not null fields

--- migrations/20220405141571_create_tag_table.up.sql
+++ migrations/20220405141571_create_tag_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tag
 (
-    id          serial NOT NULL PRIMARY KEY,
-    name        varchar(32),
-    language_id int    NOT NULL REFERENCES language (id)
+    id          serial      NOT NULL PRIMARY KEY,
+    name        varchar(32) NOT NULL,
+    language_id int         NOT NULL REFERENCES language (id)
 );
