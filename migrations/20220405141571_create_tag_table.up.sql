0434b1db9788ff73911eecfbe541a7d396e45e2c
Alex Malykh <49870662+ysomad@users.noreply.github.com>
2022-05-30 00:03:25 +0700

    ref: made tag names unique

--- migrations/20220405141571_create_tag_table.up.sql
+++ migrations/20220405141571_create_tag_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS tag
 (
     id          serial      NOT NULL PRIMARY KEY,
-    name        varchar(32) NOT NULL,
+    name        varchar(32) UNIQUE NOT NULL,
     language_id int         NOT NULL REFERENCES language (id)
 );
